c7597449e8633cfb0df651d5d9edda3b321fd793
Nikita Beletskiy <bel.nikita3197@gmail.com>
2021-02-07 14:45:24 +0300

    Initial app setup without context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import './index.css';
+import BookList from './components/BookList';
+import Navbar from './components/Navbar';
+
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Navbar />
+      <BookList />
+    </div>
+  );
 };
 
 export default App;
